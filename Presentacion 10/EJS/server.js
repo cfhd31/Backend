552b94e653b520c86c0b13e54f899ac865df5aed
cfhd31 <cfhd31@hotmail.com>
2022-02-01 20:50:02 -0300

    ejs

---
+++ Presentacion 10/EJS/server.js
@@ -0,0 +1,36 @@
+/* ----------------------- Modulos ----------------------*/
+const express = require('express');
+const productos = []
+
+
+/* ---------------- Instancia de express ----------------*/
+const app = express()
+
+
+/* -------------------- Middlewares ------------------- */
+app.use(express.urlencoded({ extended: true }))
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+/* --------------------- Rutas ------------------------*/
+app.get('/', (req, res) => {
+    res.render('inicio', { productos });
+});
+
+app.get('/resultados', (req, res) => {
+    res.render('resultados', { productos });
+});
+
+app.post('/productos', (req, res) => {
+    productos.push(req.body)
+    res.redirect('/')
+});
+
+
+/* ------------------- Servidor -----------------------*/
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${server.address().port}`)
+})
+server.on('error', error => console.log(`Error en servidor ${error}`))
