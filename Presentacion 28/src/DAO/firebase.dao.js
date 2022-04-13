d66a2f65b9474f397dc6cc050aee5d67e736a7db
cfhd31 <cfhd31@hotmail.com>
2022-04-13 20:03:56 -0300

    13/04

---
+++ Presentacion 28/src/DAO/firebase.dao.js
@@ -0,0 +1,10 @@
+
+import ContenedorFirebase from "../Container/ContenedorFirebase.js";
+
+class MensajesDAO extends ContenedorFirebase {
+  constructor() {
+    super("mensajes");
+  }
+}
+
+export default MensajesDAO;
