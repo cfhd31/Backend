e17112764e2ed3960bc9be0e8cfa2395ac66d75a
cfhd31 <cfhd31@hotmail.com>
2022-04-19 20:26:56 -0300

    19/04

---
+++ Presentacion 30/nodeServer/server.js
@@ -0,0 +1,102 @@
+/*--------------------------- Modulos ------------------------------*/
+import express from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import passport from "passport";
+import productosTestRuta from "./rutas/productosTestRuta.js";
+import autentificacionRuta from "./rutas/autentificacionRuta.js";
+import infoRuta from "./rutas/infoRuta.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import MensajesDAO from "./src/DAO/firebase.dao.js";
+import hbs from "hbs";
+import bodyParser from "body-parser";
+import apiRandomRuta from "./rutas/apiRandomRuta.js";
+
+const usuarios = []
+const mensajeClass = new MensajesDAO();
+
+
+const app = express();
+const httpServer = new createServer(app);
+const io = new Server(httpServer);
+
+
+/*----------------------------- Session ------------------------------*/
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "1234567890!@#$%^&*()",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: "auto",
+      maxAge: 60000,
+    },
+  })
+);
+
+app.use(function(req, res, next){
+  res.locals.session = req.session;
+  next();
+});
+
+const __dirname = path.resolve();
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/public/views");
+hbs.registerPartials(__dirname + "/public/views/partials", function (err) {});
+
+/*-------------------------- Passport ------------------------------*/
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/*---------------------------- Rutas --------------------------------*/
+app.use("/", autentificacionRuta);
+app.use("/", productosTestRuta);
+app.use("/info", infoRuta);
+app.use("/api/randoms", apiRandomRuta);
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
+
+/*------------------------- Mensaje Socket -------------------------*/
+io.on("connection", async (socket) => {
+  console.log(`Nuevo cliente conectado ${socket.id}`);
+
+  socket.on("mensajeNuevo", async (msg) => {
+    await mensajeClass.guardar(msg);
+    const mensajeGuardados = await mensajeClass.mostrarTodos();
+
+    const normalizar = normalizedHolding(mensajeGuardados);
+    const longO = JSON.stringify(mensajeGuardados).length;
+    const longN = JSON.stringify(normalizar).length;
+    const porcentaje = (longN * 100) / longO;
+
+    socket.emit("mensajes", {
+      normalizedHolding: normalizar,
+      porcentaje: porcentaje,
+    });
+  });
+});
+
+/* ---------------------- Servidor ----------------------*/
+const PORT = parseInt(process.argv[2]) || 8080;
+
+app.get('/', (req, res)=>{
+    res.send(`Servidor express en ${PORT} - PID ${process.pid} - ${new Date().toLocaleString()}`)
+})
+
+app.listen(PORT, err => {
+    if (!err) console.log(`Servidor express escuchando en el puerto ${PORT} - PID WORKER ${process.pid}`)
+});
+
+
+
+
+
+
