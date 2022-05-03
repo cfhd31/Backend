cbbd41f1929e8afeb702dcd3c728abf1566ea802
cfhd31 <cfhd31@hotmail.com>
2022-05-02 21:13:40 -0300

    2/05

---
+++ Presentacion 32/nodeServer/server.js
@@ -0,0 +1,84 @@
+import cookieParser   from 'cookie-parser';
+import session  from 'express-session';
+import passport  from 'passport';
+import express from 'express';
+import autentificacionRuta  from './rutas/autentificacionRuta.js';
+import productosTestRuta  from './rutas/productosTestRuta.js';
+import infoRuta  from './rutas/infoRuta.js';
+import apiRandomRuta  from './rutas/apiRandomRuta.js';
+import bodyParser  from 'body-parser';
+import hbs from 'hbs';
+import cluster from "cluster";
+import os from "os";
+import compression from 'compression' 
+import logger from './logger.js';
+
+
+const app = express();
+
+/*----------------------------- Session ------------------------------*/
+logger.info(`Conf logger modo: ${process.env.NODE_ENV}`);
+app.use(compression());
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "1234567890!@#$%^&*()",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: "auto",
+      maxAge: 60000,
+    },
+  })
+);
+
+app.use(function(req, res, next){
+  res.locals.session = req.session;
+  next();
+});
+
+import path from "path";
+const __dirname = path.resolve();
+ 
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/public/views");
+hbs.registerPartials(__dirname + "/public/views/partials", function (err) {});
+
+/*-------------------------- Passport ------------------------------*/
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*---------------------------- Rutas --------------------------------*/
+app.use("/", autentificacionRuta);
+app.use("/", productosTestRuta);
+app.use("/info", infoRuta);
+app.use("/api/random", apiRandomRuta);
+
+const numCPUs =  os.cpus().length;
+ 
+const PORT = 8085;
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+}
+else{
+  app.listen(PORT, err =>{
+    err ?
+    console.log("Error in server setup") :
+    console.log(`Worker ${process.pid} started`);
+  });
+}
+
+//PASOS A SEGUIR: 
+                  //pm2 start server.js --name="Server1" --watch -- port=8081 
+                  //pm2 start server.js --name="Server3" --watch -i max
+                  //pm2 monit
+                  //pm2 kill
